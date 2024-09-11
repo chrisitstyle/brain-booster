9f88f03ec759ff7a465712f0fa439a834349fcf4
chrisitstyle <infernos1244@int.pl>
2024-09-11 17:11:24 +0200

    Add the freshly generated backend project

---
+++ brain-booster-backend/src/main/java/com/brainbooster/BrainBoosterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brainbooster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBoosterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBoosterBackendApplication.class, args);
+	}
+
+}
