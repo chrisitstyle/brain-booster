78c4c758f1385af25921f0ea4792258d4d09116f
chrisitstyle <infernos1244@int.pl>
2024-10-09 15:53:55 +0200

    Add get all users method in user's controller and service

--- brain-booster-backend/src/main/java/com/brainbooster/controller/UserController.java
+++ brain-booster-backend/src/main/java/com/brainbooster/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.brainbooster.controller;
 
 
+import com.brainbooster.model.User;
+import com.brainbooster.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 }
--- brain-booster-backend/src/main/java/com/brainbooster/service/UserService.java
+++ brain-booster-backend/src/main/java/com/brainbooster/service/UserService.java
@@ -1,7 +1,21 @@
 package com.brainbooster.service;
 
+import com.brainbooster.model.User;
+import com.brainbooster.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
