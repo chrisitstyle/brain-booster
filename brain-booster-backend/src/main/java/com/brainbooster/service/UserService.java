2fe74188810d44f1448df25493febe42ae9791fc
chrisitstyle <infernos1244@int.pl>
2024-10-09 17:33:22 +0200

    Add addUser method in UserService

--- brain-booster-backend/src/main/java/com/brainbooster/service/UserService.java
+++ brain-booster-backend/src/main/java/com/brainbooster/service/UserService.java
@@ -3,16 +3,27 @@ package com.brainbooster.service;
 import com.brainbooster.model.User;
 import com.brainbooster.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-
+    public ResponseEntity<User> addUser(User user){
+        Optional<User> userFromDatabase = userRepository.findByEmail(user.getEmail());
+        if(userFromDatabase.isPresent()){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "User with this email already exists");
+        }
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
