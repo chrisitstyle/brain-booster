9d737e757432e629e016cb73497854a1d1d334a3
chrisitstyle <infernos1244@int.pl>
2024-10-10 19:19:54 +0200

    Add @Repository annotation in FlashcardSetRepository

--- brain-booster-backend/src/main/java/com/brainbooster/repository/FlashcardSetRepository.java
+++ brain-booster-backend/src/main/java/com/brainbooster/repository/FlashcardSetRepository.java
@@ -2,6 +2,8 @@ package com.brainbooster.repository;
 
 import com.brainbooster.model.FlashcardSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlashcardSetRepository extends JpaRepository<FlashcardSet, Long> {
 }
