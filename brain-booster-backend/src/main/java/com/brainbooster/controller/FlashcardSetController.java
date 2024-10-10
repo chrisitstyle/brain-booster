e9d7812c464931670b45a25b9cfeb00a4a90a993
chrisitstyle <infernos1244@int.pl>
2024-10-10 20:22:46 +0200

    Add FlashcardSetController with getAllFlashcardSets method

---
+++ brain-booster-backend/src/main/java/com/brainbooster/controller/FlashcardSetController.java
@@ -0,0 +1,23 @@
+package com.brainbooster.controller;
+
+import com.brainbooster.model.FlashcardSet;
+import com.brainbooster.service.FlashcardSetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/flashcardsets")
+public class FlashcardSetController {
+
+    private final FlashcardSetService flashcardSetService;
+
+    @GetMapping
+    public ResponseEntity<List<FlashcardSet>> getAllFlashcardSets() {
+        List<FlashcardSet> flashcardSets = flashcardSetService.getAllFlashcardSets();
+        return ResponseEntity.ok(flashcardSets);
+    }
+}
