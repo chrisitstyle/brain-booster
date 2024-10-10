0fd2fa60f2e7a94ebca956cfdb77de7ddc0bf6fa
chrisitstyle <infernos1244@int.pl>
2024-10-10 20:11:11 +0200

    Add @Getter @Setter annotations in FlashCardSet

--- brain-booster-backend/src/main/java/com/brainbooster/model/FlashcardSet.java
+++ brain-booster-backend/src/main/java/com/brainbooster/model/FlashcardSet.java
@@ -1,9 +1,13 @@
 package com.brainbooster.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "flashcard_set")
 public class FlashcardSet {
