56b97e2fe500a606d8782c258a26ae581d5788dc
chrisitstyle <infernos1244@int.pl>
2024-10-10 20:25:47 +0200

    Remove unnecessary imports from FlashcardSetService

--- brain-booster-backend/src/main/java/com/brainbooster/service/FlashcardSetService.java
+++ brain-booster-backend/src/main/java/com/brainbooster/service/FlashcardSetService.java
@@ -3,8 +3,6 @@ package com.brainbooster.service;
 import com.brainbooster.model.FlashcardSet;
 import com.brainbooster.repository.FlashcardSetRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
