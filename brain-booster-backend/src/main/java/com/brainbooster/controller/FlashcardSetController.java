4bcf9eaa79203074caed92d01103ed97d19d9bee
chrisitstyle <infernos1244@int.pl>
2024-10-27 22:33:52 +0100

    Add return ResponseEntity in addFlashcardSet

--- brain-booster-backend/src/main/java/com/brainbooster/controller/FlashcardSetController.java
+++ brain-booster-backend/src/main/java/com/brainbooster/controller/FlashcardSetController.java
@@ -3,6 +3,7 @@ package com.brainbooster.controller;
 import com.brainbooster.model.FlashcardSet;
 import com.brainbooster.service.FlashcardSetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,9 @@ public class FlashcardSetController {
 
     @PostMapping
     public ResponseEntity<FlashcardSet> addFlashcardSet(@RequestBody FlashcardSet flashcardSet) {
-        return flashcardSetService.addFlashcardSet(flashcardSet);
+        flashcardSetService.addFlashcardSet(flashcardSet);
+        return new ResponseEntity<>(flashcardSet, HttpStatus.CREATED);
+
     }
 
     @GetMapping
